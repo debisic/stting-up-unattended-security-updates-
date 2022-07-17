235ac115dd6a5893abf483830c24f00d8f5bda3f
obiomaokorowu <109354887+obiomaokorowu@users.noreply.github.com>
2022-07-17 09:41:52 +0100

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,28 @@
+#Update the server
+sudo apt update && sudo apt upgrade -y
+
+#Install unattended upgrades on Ubuntu.
+sudo apt install unattended-upgrades apt-listchanges bsd-mailx -y
+
+#Turn on unattended security updates, run:
+sudo dpkg-reconfigure -plow unattended-upgrades -y
+
+#Configure automatic updates, enter
+sudo vi /etc/apt/apt.conf.d/50unattended-upgrades
+
+#Set up alert email ID:
+Unattended-Upgrade::Mail "sysadmin@server1.cyberciti.biz";
+
+#Automatically reboot Ubuntu box WITHOUT CONFIRMATION for kernel updates:
+Unattended-Upgrade::Automatic-Reboot "true";
+
+#Finally edit the /etc/apt/listchanges.conf and set email ID:
+vim /etc/apt/listchanges.conf
+email_address=sysadmin@server1.cyberciti.biz
+
+#It would be best to have a local smptd (email server) that can forward or send an email. Without that, you will never get an email alert. See how to configure AWS SES with Postfix MTA or use your Gmail account to relay email from a shell prompt.
+
+#Verify that it is working by running the following command
+sudo unattended-upgrades --dry-run
+
+
